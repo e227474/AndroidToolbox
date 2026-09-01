c9b5de810d98d6a29f31bdb41e067f2d075f5821
e227474 <e227474@sbl.ch>
2026-09-01 15:17:35 +0200

    add basic UI for curl-like utility

--- app/src/main/java/com/github/e227474/androidtoolbox/MainActivity.kt
+++ app/src/main/java/com/github/e227474/androidtoolbox/MainActivity.kt
@@ -4,10 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Row // add necessary dependencies *
+import androidx.compose.material3.ElevatedButton // *
+import androidx.compose.ui.unit.dp // *
+import androidx.compose.foundation.layout.Column //*
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.input.rememberTextFieldState
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme // *
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface // *
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,7 +32,6 @@ class MainActivity : ComponentActivity() {
             AndroidToolboxTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +41,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(modifier: Modifier = Modifier) {
+    Surface(
+        color = MaterialTheme.colorScheme.background,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Column() {
+            Row(modifier = Modifier.padding(24.dp)) {
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(text = "Get Html from source url specified below.")
+                }
+                ElevatedButton(
+                    colors = ButtonDefaults.buttonColors(),
+                    onClick = { /* TODO */ }
+                ) {
+                    Text("Get HTML")
+                }
+            }
+            Row(modifier = Modifier.padding(24.dp)) {
+                OutlinedTextField(
+                    state = rememberTextFieldState(),
+                    label = { Text("Url") }
+                )
+            }
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidToolboxTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
