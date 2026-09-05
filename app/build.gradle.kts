bffa39b49057d14f33747d4b76e21f8a31080afb
e227474 <e227474@sbl.ch>
2026-09-05 22:07:48 +0200

    add predictive back support for navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.github.e227474.androidtoolbox"
         minSdk = 35
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.3"
+        versionCode = 4
+        versionName = "0.3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
